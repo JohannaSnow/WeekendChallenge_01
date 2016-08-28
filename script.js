e80ce623858d2091fe2d505f1c591d63386b7ec4
Johanna Snow <johannadsnow@gmail.com>
2016-08-28 13:00:40 -0500

    First Attempt

---
+++ script.js
@@ -0,0 +1,90 @@
+console.log (' script is sourced ');
+
+//create an empty array to hold the information
+var employeeInfo = [];
+var combinedSalary;
+
+var addEmployee = function() {
+  console.log('in addEmployeeInfo');
+
+
+
+  var firstName = document.getElementById('firstNameIn').value;
+  var lastName = document.getElementById('lastNameIn').value;
+  var idNumber = document.getElementById('idNumberIn').value;
+  var jobTitle = document.getElementById('jobTitleIn').value;
+  var annualSalary = document.getElementById('annualSalaryIn').value;
+
+
+//made a new employee object
+  var newEmployee = {
+    firstName: firstName,
+    lastName: lastName,
+    idNumber: idNumber,
+    jobTitle: jobTitle,
+    annualSalary: annualSalary
+  };
+};
+//alert user if something is left blank
+if( newEmployee.firstname === '' || newEmployee.lastName === '' || newEmployee.idNumber === '' || newEmployee.jobTitle === '' ||
+newEmployee.annualSalary === '' ||{
+alert( 'Please fill this in with correct information');
+
+else{
+clearInputs(); ///clears the boxes so it doesnt post twice //////
+  // push employees entered into program
+    employees.push(newEmployee);
+  //display employees
+  displayEmployees();
+}
+
+
+var clearInputs = function(){
+  console.log( 'in clearInputs' );
+document.getElementById('firstNameIn').value='';
+document.getElementById('lastNameIn').value='';
+document.getElementById('idNumberIn').value='';
+document.getElementById('jobTitleIn').value='';
+document.getElementById('annualSalaryIn').value='';
+}; //end clearInput
+
+var displayEmployees = function(){
+  console.log( 'inDisplayEmployees');
+  console.log( Weekend Challenge );
+  // empty our div element
+  document.getElementById( 'allEmployees' ).innerHTML='';
+
+  for( var i = 0; i < employees.length; i++){
+    //Employee information (name, id number, salary)
+    var employeeInfo =  '<h2>' + employee[ i ].firstName+ ' ' + employee[ i ].lastName + ' ' + employee[ i ].idNumber + '' + employee[i].jobTitle + '' + employee[i].annualSalary +'' + '<button onClick="takeOut(' + i + ')">Remove Employee</button></p>';
+    // append employee info to output div
+  document.getElementById( 'allEmployees' ).innerHTML += employeeInfo;
+  } // end for
+   // end employeeInfo
+
+  var removeEmployee = function( index ){
+    console.log( 'taking out the ' + employee[ index ].firstName + ' ' + employee[ index ].lastName + '' + employee[ index ].idNumber + '' + employee[ index ].jobTitle);
+    //splice the employee at this index from out array
+   //ARRAY.splice( Index, NumbertoRemove)
+   employee.splice( index, 4 );
+   //show employees on DOM
+   displayEmployees();
+ }; //end removeEmployee
+
+  // Calculate combined employee salary/cost to the company
+  var calculateSalary = function(){
+    console.log('in calculateSalary');
+    combinedSalary = 0;
+    for (var i = 0; i < employees.length; i++) {
+      combinedSalary += Number(employees[i].annualSalary);
+    }
+    console.log(combinedSalary);
+    return combinedSalary;
+  }; // end calculateSalary
+
+
+
+
+
+// for (var i = 0; i < employeeInfo.length; i++) {
+//   employeeInfo[i]
